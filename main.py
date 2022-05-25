76eb02758a6e7a53a88b47a5d952da30ba2f898b
arturk22 <alencar.artur00@gmail.com>
2022-05-25 15:13:28 -0300

    ler, convert, zip

--- main.py
+++ main.py
@@ -16,3 +16,10 @@ def baixar_arquivo(url, endereco):
 if __name__ == "__main__":
     baixar_arquivo('https://www.gov.br/ans/pt-br/arquivos/assuntos/consumidor/o-que-seu-plano-deve-cobrir/Anexo_I_Rol_2021RN_465.2021_RN473_RN478_RN480_RN513_RN536.pdf', 'Anexo 1.pdf')
 
+arquivo = tabula.read_pdf("Anexo 1.pdf", pages="all")
+
+tabula.convert_into("Anexo 1.pdf", "Anexo 1.csv", output_format="csv", pages='all')
+
+z = zipfile.ZipFile('TableAnexos.zip', 'w', zipfile.ZIP_DEFLATED)
+z.write('Anexo 1.csv')
+z.close()
